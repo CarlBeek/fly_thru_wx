debd6b0dfd8f219be219ef684b84b02247d31dcf
Carl Beekhuizen <carl.beek@gmail.com>
2018-01-14 23:40:28 +0100

    Updates file to use new shinny json with timestamps built in.

--- wx_prob.py
+++ wx_prob.py
@@ -4,6 +4,7 @@ import mbd_util as u
 import ast
 
 sc = SparkContext("local", "wx_prob")
+sc.setLogLevel('ERROR')
 sqlContext = SQLContext(sc)
 
 df = sqlContext.read.json("hdfs:///user/s1638696/wx_data/*")
@@ -12,4 +13,5 @@ sigmets = df.flatMap(lambda r: r.features).filter(lambda r: r.properties.geom ==
 sigmets = sigmets.filter(lambda s: type(ast.literal_eval(s.geometry.coordinates[0])) == list)
 wx_blocks = sigmets.flatMap(lambda s: [(block, [s]) for block in u.get_covered_blocks(ast.literal_eval(s.geometry.coordinates[0]))])
 wx_per_block = wx_blocks.reduceByKey(lambda a, b: a + b)
-wx_time_per_block = wx_per_block.map(lambda (block, list_of_sigmets): (block, sum([u.time_string_to_timestamp(s.properties.validTimeTo) -  u.time_string_to_timestamp(s.properties.validTimeFrom) for s in list_of_sigmets])))
+wx_time_per_block = wx_per_block.map(lambda (block, list_of_sigmets): (block, sum([s.properties.v_to -  s.properties.v_from for s in list_of_sigmets])))
+
