d836c38fa086823bbfa4326d99126db62e87cdb5
s1542648 <s1542648@wegdam.ewi.utwente.nl>
2018-01-15 15:54:06 +0100

    Finished the wx_prob script. It now writes to carls directory

--- wx_prob.py
+++ wx_prob.py
@@ -9,9 +9,16 @@ sqlContext = SQLContext(sc)
 
 df = sqlContext.read.json("hdfs:///user/s1638696/wx_data/*")
 
-sigmets = df.flatMap(lambda r: r.features).filter(lambda r: r.properties.geom == "AREA")
+
+def calc_wx_prob(kv):
+	block, t = kv
+	return block, t/(365*24*3600*1000.0)
+
+sigmets = df.flatMap(lambda r: r.features).filter(lambda r: r.properties.geom == "AREA").filter(lambda r: not (r.properties.v_from is None or r.properties.v_to is None))
 sigmets = sigmets.filter(lambda s: type(ast.literal_eval(s.geometry.coordinates[0])) == list)
 wx_blocks = sigmets.flatMap(lambda s: [(block, [s]) for block in u.get_covered_blocks(ast.literal_eval(s.geometry.coordinates[0]))])
 wx_per_block = wx_blocks.reduceByKey(lambda a, b: a + b)
 wx_time_per_block = wx_per_block.map(lambda (block, list_of_sigmets): (block, sum([s.properties.v_to -  s.properties.v_from for s in list_of_sigmets])))
+wx_prob = wx_time_per_block.map(calc_wx_prob)
+wx_prob.toDF().write.json("/user/s1638696/ac_output/wx_prob.json", mode="overwrite")
 
