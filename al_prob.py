d1090480048583024d94d6c4b9e52339f2465273
s1542648 <s1542648@farm05.ewi.utwente.nl>
2018-01-16 12:44:25 +0100

    Fixed al_prob and added different weather types to wx_prob

--- al_prob.py
+++ al_prob.py
@@ -3,16 +3,15 @@ from pyspark.sql import SQLContext
 import mbd_util as u
 
 
-sc = SparkContext(appName="wx_prob")
+sc = SparkContext(appName="al_prob")
 sc.setLogLevel('ERROR')
 sqlContext = SQLContext(sc)
 
-def reduce_airlines(agg, new_value):
-	for k, v in new_value.items():
-		if k in agg:
-			agg[k] += v
-		else:
-			agg[k] = v
+def aggregate_airlines(agg, airline):
+	if airline in agg:
+		agg[airline] += 1
+	else:
+		agg[airline] = 1
 	return agg
 	
 def al_probs(b, d):
@@ -27,12 +26,20 @@ def al_probs(b, d):
 		output.append( (((b_lat, b_lon), k), v/ac_in_b) )
 	return output
 
-df = sqlContext.read.json("file:///home/s1638696/flight_data/2017-12-19-small.tar.gz")
-#df = sqlContext.read.json("hdfs:///user/s1638696/flight_data/*")
-airlines_per_block = df.rdd.filter(lambda r: not (r.Lat is None or r.Long is None or r.Op is None)).map(lambda r: (u.block_name(r.Lat, r.Long), {r.Op: 1}))
+def combine_dicts(d1, d2):
+	for k, v in d2.items():
+		if k in d1:
+			d1[k] += d2[k]
+		else:
+			d1[k] = d2[k]
+	return d1
+
+#df = sqlContext.read.json("file:///home/s1638696/flight_data/2017-12-19-small.tar.gz")
+df = sqlContext.read.json("hdfs:///user/s1638696/flight_data/*")
+airlines_per_block = df.rdd.filter(lambda r: not (r.Lat is None or r.Long is None or r.Op is None)).map(lambda r: (u.block_name(r.Lat, r.Long), r.Op))
 # This should be aggregate by key. Aggregate operates on partitions instead of combining values accross partitions
-red_al = airlines_per_block.reduceByKey(reduce_airlines)
+red_al = airlines_per_block.aggregateByKey(dict(), aggregate_airlines, combine_dicts)
 al_prob = red_al.flatMap(lambda (b, d): al_probs(b,d)) 
 
-al_prob.toDF().write.json("file:///home/s1542648/al_prob.json", mode="overwrite")
-#al_prob.toDF().write.json("/user/s1638696/ac_output/al_prob.json", mode="overwrite")
+#al_prob.toDF().write.json("file:///home/s1542648/al_prob.json", mode="overwrite")
+al_prob.toDF().write.json("/user/s1638696/ac_output/al_prob.json", mode="overwrite")
--- wx_prob.py
+++ wx_prob.py
@@ -3,10 +3,11 @@ from pyspark.sql import SQLContext
 import mbd_util as u
 import ast
 
-sc = SparkContext("local", "wx_prob")
+sc = SparkContext(appName="wx_prob")
 sc.setLogLevel('ERROR')
 sqlContext = SQLContext(sc)
 
+#df = sqlContext.read.json("file:///home/s1542648/wx_data/20171229.json")
 df = sqlContext.read.json("hdfs:///user/s1638696/wx_data/*")
 
 
@@ -16,9 +17,10 @@ def calc_wx_prob(kv):
 
 sigmets = df.flatMap(lambda r: r.features).filter(lambda r: r.properties.geom == "AREA").filter(lambda r: not (r.properties.v_from is None or r.properties.v_to is None))
 sigmets = sigmets.filter(lambda s: type(ast.literal_eval(s.geometry.coordinates[0])) == list)
-wx_blocks = sigmets.flatMap(lambda s: [(block, [s]) for block in u.get_covered_blocks(ast.literal_eval(s.geometry.coordinates[0]))])
+wx_blocks = sigmets.flatMap(lambda s: [((block, s.properties.hazard), [s]) for block in u.get_covered_blocks(ast.literal_eval(s.geometry.coordinates[0]))])
 wx_per_block = wx_blocks.reduceByKey(lambda a, b: a + b)
-wx_time_per_block = wx_per_block.map(lambda (block, list_of_sigmets): (block, sum([s.properties.v_to -  s.properties.v_from for s in list_of_sigmets])))
+wx_time_per_block = wx_per_block.map(lambda (k, list_of_sigmets): (k, sum([s.properties.v_to -  s.properties.v_from for s in list_of_sigmets])))
 wx_prob = wx_time_per_block.map(calc_wx_prob)
 wx_prob.toDF().write.json("/user/s1638696/ac_output/wx_prob.json", mode="overwrite")
+#wx_prob.toDF().write.json("file:///home/s1542648/wx_data/output", mode="overwrite")
 
